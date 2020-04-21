246569b5bd0548d3c5cf2eb4d188a47593dfb9f0
Jose <jj.lopezbuilder@gmail.com>
2020-04-21 14:27:51 -0500

    Password Generator was implented and is operational.

--- random_password_generator/urls.py
+++ random_password_generator/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 
     path('',views.rpg, name="rpg"),
-
+    path('about_us',views.about_us, name= 'about_us'),
+    path('password',views.password, name= 'password'),
 
 ]
--- random_password_generator/views.py
+++ random_password_generator/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
+
 # Create your views here.
 def rpg(request):
     return render(request, 'random_password_generator/rpg.html',{})
+
+
+def about_us(request):
+    return render(request, 'random_password_generator/about_us.html')
+
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYS'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*'))
+
+    length = int(request.GET.get('length',12))
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'random_password_generator/password.html', {'password':thepassword})
